c80a8baf0fbf10d299cff1782c42e24eb3a7c63f
Asma-Abed <abed.asma@hotmail.com>
2023-07-28 11:01:16 +0100

    member schema added

---
+++ models/memberModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const memberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide your name!'],
+  },
+  photo: String,
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email!'],
+  },
+  job: String,
+  manager: {
+    type: Boolean,
+    default: false,
+  },
+  bio: String,
+  social: [String],
+  password: {
+    type: String,
+    required: [true, 'Please provide a password!'],
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'Please confirm your password!'],
+  },
+});
+
+const Member = mongoose.model('Member', memberSchema);
+module.exports = Member;
