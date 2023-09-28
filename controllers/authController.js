3c5b59d2c9cb86b22c0267038dbb9d4c95fe6ea2
Asma-Abed <abed.asma@hotmail.com>
2023-09-28 11:44:35 +0100

    signing up users implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,22 @@
+const jwt = require('jsonwebtoken');
 const Member = require('../models/memberModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newMember = await Member.create(req.body);
+  const newMember = await Member.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  });
+
+  const token = jwt.sign({ id: newMember._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       newMember,
     },
