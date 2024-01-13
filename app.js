f8c2b9680d36518eaf0391ef6b6514b5bd988618
Asma-Abed <abed.asma@hotmail.com>
2024-01-13 17:19:11 +0100

    rate limiting added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const clubRouter = require('./routes/clubRoutes');
 const bookRouter = require('./routes/bookRoutes');
@@ -10,6 +11,14 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP! Please try again in one hour',
+});
+
+app.use('/api', limiter);
 
 app.use('/api/v1/clubs', clubRouter);
 app.use('/api/v1/books', bookRouter);
